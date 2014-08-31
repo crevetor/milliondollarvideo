66a7e8ba0ea57919b4457f4af2d09cbca2ae96fa
Antoine Reversat <a.reversat@gmail.com>
2014-08-31 19:04:29 +0200

    Create vtt file and correctly resize video

--- mdv/scripts/concat_all.py
+++ mdv/scripts/concat_all.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import sys
 import logging
+from datetime import time
 from django.conf import settings
 from moviepy.editor import *
 
@@ -10,6 +11,9 @@ sys.path.append("../")
 from videoupload.models import VideoFragment
 
 def process_videos(vid_out, text_out):
+    vtt = open(text_out, "w")
+    vtt.write("WEBVTT\n\n")
+
     clips = []
     starts = VideoFragment.objects.filter(prev_video=None)
     if len(starts) > 1:
@@ -17,6 +21,7 @@ def process_videos(vid_out, text_out):
                         prev_video")
 
     start = starts.get()
+    timestamp = 0.0
 
     while True:
         logging.debug("Cating file %s", os.path.join(settings.MEDIA_ROOT,
@@ -24,12 +29,22 @@ def process_videos(vid_out, text_out):
         clip = VideoFileClip(os.path.join(settings.MEDIA_ROOT,
                                                 start.vidfile.name))
         clips.append(clip)
+
+        vtt.write("%02d:%06.3f --> %02d:%06.3f\n" % (timestamp / 60,
+                                                   timestamp % 60,
+                                                   (timestamp + clip.duration) / 60,
+                                                   (timestamp + clip.duration) % 60))
+        vtt.write("%s|%s\n\n" % (start.id, start.link))
+
+
+        timestamp += clip.duration
         start = start.next_video
         if not start:
             break
 
-    output = concatenate(clips)
-    output.size = (800, 600)
+    vtt.close()
+
+    output = concatenate(clips).fx(vfx.resize, width=800, height=600)
     output.write_videofile(vid_out)
 
 
